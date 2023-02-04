1be0019ffdf0f92615a1138b8021baf1c9472d54
超杰 <1098626505@qq.com>
2023-02-04 21:26:02 +0800

    fix: github page muti page

---
+++ scripts/gen-html.ts
@@ -0,0 +1,16 @@
+import path from 'path'
+import fg from 'fast-glob'
+import fs from 'fs-extra'
+
+const docsDist = path.join(__dirname, '../packages/docs/dist')
+const indexHtmlPath = path.join(docsDist, '/index.html')
+
+fg.sync(['*/**/index.md', '*/**/README.md'], {
+  cwd: path.join(__dirname, '../packages'),
+  onlyFiles: true,
+  ignore: ['**/node_modules', '**/dist', 'docs', 'solid-to-vue']
+})
+  .map(item => item.replace('/src', '').replace('/index.md', '').replace('.md', ''))
+  .forEach(item => {
+    fs.createLink(indexHtmlPath, path.join(docsDist, `${item}.html`))
+  })
